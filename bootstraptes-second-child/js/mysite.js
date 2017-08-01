812ab1288c0e34548c5e7dca182d05d5d7d150b3
eshasoftwareseni <30397171+eshasoftwareseni@users.noreply.github.com>
2017-08-01 16:53:08 +0700

    Update mysite.js
    Update mysite.js

--- bootstraptes-second-child/js/mysite.js
+++ bootstraptes-second-child/js/mysite.js
@@ -1,13 +1,12 @@
 jQuery(document).ready(function($) {	
 	
-	$('#sul-name').autoComplete({
-		minChars: 1,
+	$('#sul-name').autocomplete({
 		source: function(sulname, response) {
 			$.ajax({
 				type: 'POST',
 				dataType: 'json',
 				url: object_name.url,
-				data: object_name.data+sulname,
+				data: object_name.data+sulname.term,
 				success: function(data) {
 					response(data);
 				}
